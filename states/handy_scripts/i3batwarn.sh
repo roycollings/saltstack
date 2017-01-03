499f1394c3b7ad8175c5eadf0a75f57b620dbb57
Roy Collings <roy.collins@postcodelottery.co.uk>
2017-01-03 17:23:42 +0000

    Added battery warning script (stilll need to get it cronned automatically)

--- states/handy_scripts/i3batwarn.sh
+++ states/handy_scripts/i3batwarn.sh
@@ -1,25 +1,18 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
+# Add the folowing line to check battery status every minute
+# */1 * * * * /PATH/TO/YOUR/SCRIPT/i3batwarn.sh
 
-#############################################
-# This is a simple battery warning script.  #
-# It uses i3's nagbar to display warnings.  #
-#                                           #
-# @author agribu                            #
-#############################################
-
-# lock file location
 export LOCK_FILE=/tmp/battery_state.lock
-
-# check if another copy is running
-if [[ -a $LOCK_FILE ]]; then
-
+if [[ -a $LOCK_FILE ]]
+then
     pid=$(cat $LOCK_FILE | awk '{print $1}')
 	ppid=$(cat $LOCK_FILE | awk '{print $2}')
-	# validate contents of previous lock file
+	
 	vpid=${pid:-"0"}
 	vppid=${ppid:-"0"}
 
-    if (( $vpid < 2 || $vppid < 2 )); then
+    if (( $vpid < 2 || $vppid < 2 ))
+    then
 		# corrupt lock file $LOCK_FILE ... Exiting
 		cp -f $LOCK_FILE ${LOCK_FILE}.`date +%Y%m%d%H%M%S`
 		exit
@@ -35,39 +28,20 @@ if [[ -a $LOCK_FILE ]]; then
 		# bogus lock file found, removing
 		rm -f $LOCK_FILE >/dev/null
 	fi
-
 fi
-
 pid=$$
 ps -f -p $pid --no-headers | awk '{print $2,$3}' > $LOCK_FILE
-# starting with process id $pid
 
-# set Battery
 BATTERY=$(ls /sys/class/power_supply/ | grep '^BAT')
-
-# set full path
 ACPI_PATH="/sys/class/power_supply/$BATTERY"
-
-# get battery status
 STAT=$(cat $ACPI_PATH/status)
-
-# get remaining energy value
 REM=`grep "POWER_SUPPLY_ENERGY_NOW" $ACPI_PATH/uevent | cut -d= -f2`
-
-# get full energy value
 FULL=`grep "POWER_SUPPLY_ENERGY_FULL_DESIGN" $ACPI_PATH/uevent | cut -d= -f2`
-
-# get current energy value in percent
 PERCENT=`echo $(( $REM * 100 / $FULL ))`
+LIMIT="15"
+MESSAGE="WARNING: Battery below ${LIMIT}%!"
 
-# set error message
-MESSAGE="** WARNING: Battery low! **"
-
-# set energy limit in percent, where warning should be displayed
-LIMIT="10"
-
-# show warning if energy limit in percent is less then user set limit and
-# if battery is discharging
-if [ $PERCENT -le "$(echo $LIMIT)" ] && [ "$STAT" == "Discharging" ]; then
-    DISPLAY=:0.0 /usr/bin/i3-nagbar -m "$(echo $MESSAGE)" -t warning
+if [ $PERCENT -le "$(echo $LIMIT)" ] && [ "$STAT" == "Discharging" ]
+then
+    DISPLAY=:0.0 /usr/bin/i3-nagbar -m "$(echo $MESSAGE)" -t error
 fi
